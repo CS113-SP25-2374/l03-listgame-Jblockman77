e6559cc2f06f1e41e51ed260c13fe9ea61ade083
Rafail Lishman <rafaillishman@yahoo.com>
2025-02-22 15:33:17 -0500

    I forgot how to implemennt the arraylist into the game....

--- src/main/java/cs113/listGame/enemies/EnemyArrayList.java
+++ src/main/java/cs113/listGame/enemies/EnemyArrayList.java
@@ -1,19 +1,42 @@
 package cs113.listGame.enemies;
 
 import cs113.listGame.behaviors.Behavior;
+import cs113.listGame.behaviors.FollowObject;
 import cs113.listGame.gamecore.GameObject;
+import cs113.listGame.behaviors.MoveToPoint;
+import cs113.listGame.behaviors.Waiting;
+import javafx.geometry.Point2D;
 import javafx.scene.image.ImageView;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class EnemyArrayList extends EnemyObject {
+    Random random = new Random();
+    ArrayList<Behavior> behaviors = new ArrayList<Behavior>();
+
     public EnemyArrayList(ImageView imageView) { super(imageView); }
 
     @Override
     public void seedBehaviors() {
+        Point2D dest = new Point2D(random.nextDouble(500), random.nextDouble(400));
+
+        behaviors.add(new Waiting(this, 2000));
+        behaviors.add(new MoveToPoint(this, 1500, dest));
 
     }
 
     @Override
     public Behavior getNextBehavior() {
-        return null;
+
+        if(behaviors.isEmpty()){
+            return null;
+        }
+
+        int index = random.nextInt(behaviors.size());
+        Behavior behavior = behaviors.get(index);
+        behaviors.set(index, null);
+        return behavior;
     }
 }
--- src/main/java/cs113/listGame/resources/ResourceArrayList.java
+++ src/main/java/cs113/listGame/resources/ResourceArrayList.java
@@ -1,25 +1,35 @@
 package cs113.listGame.resources;
 
 import cs113.listGame.gamecore.GameObject;
+import java.util.ArrayList;
 
 public class ResourceArrayList implements ResourceList {
+    ArrayList<ResourceObject> resources = new ArrayList<ResourceObject>();
     @Override
     public void add(ResourceObject resource) {
-
+        resources.add(resource);
     }
 
     @Override
     public void remove(ResourceObject resource) {
-
+        resources.remove(resource);
     }
 
     @Override
     public void truncate(ResourceObject resource) {
-
+        int count =resources.indexOf(resource);
+        for(int i=0; i<resources.size(); i++){
+            resources.remove(count);
+        }
     }
 
     @Override
     public void follow(GameObject leader) {
-
+        for(int i = 0; i < resources.size(); i++){
+            if(resources.get(i) != null){
+                resources.get(i).moveTowards(leader.getEchoCenter());
+                leader = resources.get(i);
+            }
+        }
     }
 }
